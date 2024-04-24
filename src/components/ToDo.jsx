1fe1b0c7708abdf3e959c5ea6866919be6b55598
nishanthan-k <krn.nishanthan@gmail.com>
2024-04-24 23:40:26 +0530

    update input focus using ref

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToDo, editToDo, updateToDoInput, updateEditToDoID } from "../features/todo/todoSlice";
+import {
+  addToDo,
+  editToDo,
+  updateToDoInput,
+  updateEditToDoID,
+} from "../features/todo/todoSlice";
 import TodoState from "./ToDoState";
 
 function ToDo() {
   const input = useSelector((state) => state.inputToDo);
   const editToDoID = useSelector((state) => state.editToDoID);
   const buttonContext = editToDoID ? "Update" : "Create";
+  const inputRef = useRef();
   const dispatch = useDispatch();
+
+  // when component mounting and editToDoID changing
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [editToDoID])
 
   const addToDoHandler = (e) => {
     e.preventDefault();
@@ -20,6 +31,9 @@ function ToDo() {
       }
       // Reset editToDoID after adding or updating todo
       dispatch(updateEditToDoID({ id: null }));
+
+      // Reset input after adding or updating todo
+      dispatch(updateToDoInput({ task: "" }));
     }
   };
 
@@ -31,6 +45,7 @@ function ToDo() {
           name="input"
           id="input"
           value={input || ""}
+          ref={inputRef}
           className="outline-none px-2 w-5/6 "
           onChange={(e) => dispatch(updateToDoInput({ task: e.target.value }))}
         />
